b40cd06951052d74c7e3b55f8d9cd26e7bf5948c
Ennak-alt <aj72kanne@gmail.com>
2022-08-01 22:59:27 +0200

    Did EOF exercise

---
+++ c4/Exercise 4-3-9/main.c
@@ -0,0 +1,223 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#define MAXOP   100
+#define NUMBER  '0'
+#define VARMAX  26
+#define VAR     '1'
+
+#define TOP       'T'
+#define DUPLICATE 'D'
+#define SWAP      'W'
+#define CLEAR     'C'
+#define SIN       'S'
+#define EXP       'X'
+#define POW       '^'
+
+int getop(char []);
+void push(double);
+double pop(void);
+void clear(void);
+
+int main() {
+  int type;
+  double op2, num, sndNum;
+
+  char s[MAXOP];
+
+  double vars[VARMAX];
+  int taken[VARMAX] = {0};
+
+  double lp = 0;
+
+  while ((type = getop(s)) != EOF) {
+    switch(type) {
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if (op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '%':
+        op2 = pop();
+        push(fmod(pop(), op2));
+        break;
+      case '^':
+        op2 = pop();
+        push(powf(pop(), op2));
+        break;
+      case '#':
+        push(lp);
+        break;
+      case SIN:
+        push(sin(pop()));
+        break;
+      case EXP:
+        push(exp(pop()));
+        break;
+      case TOP: 
+        num = pop();
+        printf("\tTop: %.8g\n", num);
+        push(num);
+        break;
+      case DUPLICATE:
+        num = pop();
+        push(num);
+        push(num);
+        break;
+      case SWAP:
+        num = pop();
+        sndNum = pop();
+        push(num);
+        push(sndNum);
+        break;
+      case CLEAR:
+        clear();
+        break;
+      case '\n':
+        printf("\t%.8g\n", lp = pop());
+        break;
+      default:
+        if (islower(type)) {
+          int index = type - 'a';
+          if (taken[index])
+            push(vars[index]);
+          else  {
+            taken[index] = 1;
+            vars[index] = pop();
+          } 
+          break;
+        }
+        else 
+          printf("error: unknown command %s\n", s);
+        break;
+    }
+  }
+  return 0;
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+  if (sp < MAXVAL)
+    val[sp++] = f;
+  else 
+    printf("error: stack full, can't push %g\n", f);
+}
+
+void clear(void) {
+  for (;sp > 0; sp--)
+    pop();
+}
+
+double pop(void) {
+  if (sp > 0)
+    return val[--sp];
+  else {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[]) {
+  int i, c;
+  while ((s[0] = c = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+  if (!isdigit(c) && c != '.') 
+    return c;
+  i = 0;
+  if (isdigit(c))
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  if (c == '.')
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  s[i] = '\0';
+  if (c != EOF)
+    ungetch(c);
+  return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+char end = 0;
+
+char bufc = -1;
+
+int getch(void) {
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+// int getchv2(void) {
+//   int tmp;
+//   return ((tmp = bufc) != -1) ? bufc = -1, tmp : getchar();
+// }
+
+// int getchv3(void) {
+//   if (end)
+//     return EOF;
+//   int tmp;
+//   tmp = ((tmp = bufc) != -1) ? bufc = -1, tmp : getchar();
+//   if (tmp == EOF)
+//     end = 1;
+//   return tmp;
+// }
+
+void ungetch(int c) {
+  if (bufp >= BUFSIZE) 
+    printf("ungetch: too many characters\n");
+  else 
+    buf[bufp++] = c;
+}
+
+// void ungetcv2(int c) {
+//   if (bufc == -1)
+//     bufc = c;
+//   else 
+//     printf("ungetch: already filled\n");
+// }
+
+// void ungetcv3(int c) {
+//   if (end)
+//     return;
+//   if (bufc == -1)
+//     bufc = c;
+//   else 
+//     printf("ungetch: already filled\n");
+// }
+
+void ungets(char s[]) {
+  int totallength = strlen(s) + bufp;
+  if (totallength >= BUFSIZE) 
+    printf("ungets: too many characters\n");
+  else 
+    for (int i = 0; bufp < totallength; bufp++, i++)
+      buf[bufp] = s[i];
+}
---
+++ c4/Exercise 4-3-9/txt.md
@@ -0,0 +1,11 @@
+
+#4-7
+Write a routine `ungets(s)` that will push back an entire string onto the input. Should `ungets` know 
+about `buf` and `bufp`, or should it just use `ungetch`?
+**While just using ungetch might be easier, it will decrease the speed, and output more error messages 
+than necessary. Without ungets knowing how much of the buf is filled, it will try to ungets a string
+that is too large for the both, witch will result in a lot of error-messages that are uneeded, but it
+will also result in more iterations thatn necessary.**
+
+Yea, 4-8-9 have been done a bit half-assed, not tested at all.
+
