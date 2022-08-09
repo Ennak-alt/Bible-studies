a3f9c3d273ba507ea726174be5b362bd720fd485
Ennak-alt <aj72kanne@gmail.com>
2022-08-09 17:37:29 +0200

    Completed recursive version of the reverse

---
+++ c4/Exercise 4-13/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+void test(char[], char[], char[]);
+
+void reverse(char[]);
+
+int main() {
+  char str[] = "Try not. Do or do not. There is no try";
+
+  reverse(str);
+
+  test("TEST_1", "yrt on si erehT .ton od ro oD .ton yrT", str);
+
+  return 0;
+}
+
+void reverse(char str[]) {
+  static int i = 0, j;
+
+  if (i == 0)
+    j = strlen(str) - 1;
+
+  if (i > j) 
+    return;
+
+  char c = str[i];
+
+  str[i++] = str[j];
+  str[j--] = c;
+
+  reverse(str);
+}
+
+
+void test(char testName[], char expected[], char ouput[]) {
+  if (strcmp(expected, ouput) == 0) {
+    printf("\033[0m%s: \033[0;32mSUCEEDED \n", testName);
+  } else {
+    printf("\033[0m%s: \033[0;31mFAILED \n", testName);
+    printf("\033[0;31m   Expected: \"%s\" but got \"%s\" \n", expected, ouput);
+  }
+}
---
+++ c4/Exercise 4-13/txt.md
@@ -0,0 +1,2 @@
+Write a recursive version of the function reverse(s), which reversese the string s in place.
+
